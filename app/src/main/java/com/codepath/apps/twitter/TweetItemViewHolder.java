a366aa8b96e34a221fd498c1258ac24ebbbb1812
pbabu <prabhu.babu@elementum.com>
2016-08-05 00:22:45 -0700

    added support for relative time stamp

--- app/src/main/java/com/codepath/apps/twitter/TweetItemViewHolder.java
+++ app/src/main/java/com/codepath/apps/twitter/TweetItemViewHolder.java
@@ -13,11 +13,14 @@ public class TweetItemViewHolder extends RecyclerView.ViewHolder {
     public ImageView ivUserProfileImage;
     public TextView tvUserHandle;
     public TextView tvTweetText;
-
+    public TextView tvRelativeTimeStamp;
+    public TextView tvName;
 
     public TweetItemViewHolder(View view) {
         super(view);
         ivUserProfileImage = (ImageView)view.findViewById(R.id.ivUserProfileImage);
+        tvName = (TextView)view.findViewById(R.id.tvName);
+        tvRelativeTimeStamp = (TextView)view.findViewById(R.id.tvRelativeTimeStamp);
         tvUserHandle = (TextView)view.findViewById(R.id.tvUserHandle);
         tvTweetText = (TextView)view.findViewById(R.id.tvTweetText);
     }
--- app/src/main/java/com/codepath/apps/twitter/adapters/TweetsAdapter.java
+++ app/src/main/java/com/codepath/apps/twitter/adapters/TweetsAdapter.java
@@ -2,6 +2,7 @@ package com.codepath.apps.twitter.adapters;
 
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.text.format.DateUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,7 +12,10 @@ import com.codepath.apps.twitter.R;
 import com.codepath.apps.twitter.TweetItemViewHolder;
 import com.codepath.apps.twitter.models.Tweet;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.List;
+import java.util.Locale;
 
 /**
  * Created by pbabu on 8/4/16.
@@ -44,7 +48,9 @@ public class TweetsAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>
         Tweet tweet = mTweets.get(position);
         TweetItemViewHolder tweetItemViewHolder = (TweetItemViewHolder)holder;
         tweetItemViewHolder.tvTweetText.setText(tweet.getBody());
+        tweetItemViewHolder.tvName.setText(tweet.getUser().getName());
         tweetItemViewHolder.tvUserHandle.setText("@"+tweet.getUser().getScreenName());
+        tweetItemViewHolder.tvRelativeTimeStamp.setText(getRelativeTimeAgo(tweet.getCreatedAt()));
         Glide.with(mContext)
                 .load(tweet.getUser().getProfileImageUrl())
                 .fitCenter()
@@ -60,5 +66,22 @@ public class TweetsAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>
     public void clear(){
         mTweets.clear();
         notifyDataSetChanged();
+    }
+
+    public String getRelativeTimeAgo(String rawJsonDate) {
+        String twitterFormat = "EEE MMM dd HH:mm:ss ZZZZZ yyyy";
+        SimpleDateFormat sf = new SimpleDateFormat(twitterFormat, Locale.ENGLISH);
+        sf.setLenient(true);
+
+        String relativeDate = "";
+        try {
+            long dateMillis = sf.parse(rawJsonDate).getTime();
+            relativeDate = DateUtils.getRelativeTimeSpanString(dateMillis,
+                    System.currentTimeMillis(), DateUtils.SECOND_IN_MILLIS).toString();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        return relativeDate;
     }
 }
