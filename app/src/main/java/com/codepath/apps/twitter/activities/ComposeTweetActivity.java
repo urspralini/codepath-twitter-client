5fe39406d5cb88b8b4ca69a07955292fa11d01de
pbabu <prabhu.babu@elementum.com>
2016-08-06 13:54:10 -0700

    post tweet and return the result tweet to list activity

--- app/src/main/java/com/codepath/apps/twitter/activities/ComposeTweetActivity.java
+++ app/src/main/java/com/codepath/apps/twitter/activities/ComposeTweetActivity.java
@@ -4,21 +4,25 @@ import android.content.Intent;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 
 import com.codepath.apps.twitter.Constants;
 import com.codepath.apps.twitter.R;
+import com.codepath.apps.twitter.RestApplication;
 import com.codepath.apps.twitter.beans.NewTweet;
+import com.codepath.apps.twitter.clients.TwitterClient;
 import com.codepath.apps.twitter.databinding.ActivityComposeBinding;
 import com.codepath.apps.twitter.models.Tweet;
 import com.codepath.apps.twitter.models.User;
+import com.loopj.android.http.JsonHttpResponseHandler;
 import com.squareup.picasso.Picasso;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Locale;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
+import cz.msebera.android.httpclient.Header;
 import jp.wasabeef.picasso.transformations.RoundedCornersTransformation;
 
 public class ComposeTweetActivity extends AppCompatActivity {
@@ -27,6 +31,7 @@ public class ComposeTweetActivity extends AppCompatActivity {
     private ImageView mIvComposeUserProfile;
     private NewTweet mNewTweet = new NewTweet();
     private User mCurrentUser;
+    private TwitterClient mClient = RestApplication.getRestClient();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,25 +48,48 @@ public class ComposeTweetActivity extends AppCompatActivity {
                 .into(mIvComposeUserProfile);
     }
 
-
     public void onClickTweet(View view){
-        Intent resultIntent = new Intent();
         if(!mNewTweet.isEmpty()){
-            Tweet tweet = new Tweet();
-            tweet.setUser(mCurrentUser);
-            tweet.setBody(mNewTweet.text.get());
-            tweet.setCreatedAt(getDateAsString(new Date()));
+            postTweet();
+        }
+    }
+
+    public void onClickCancel(View view) {
+        sendResult(null);
+    }
+
+    private void postTweet(){
+        mClient.postTweet(mNewTweet.text.get(), new JsonHttpResponseHandler(){
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject jsonObject) {
+                Log.d("DEBUG", "timeline: " + jsonObject.toString());
+                Tweet tweet = new Tweet(jsonObject);
+                sendResult(tweet);
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+                super.onFailure(statusCode, headers, responseString, throwable);
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONArray errorResponse) {
+                super.onFailure(statusCode, headers, throwable, errorResponse);
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+                super.onFailure(statusCode, headers, throwable, errorResponse);
+            }
+        });
+    }
+
+    private void sendResult(Tweet tweet){
+        Intent resultIntent = new Intent();
+        if(tweet != null) {
             resultIntent.putExtra(Constants.NEW_TWEET_KEY, tweet);
         }
         setResult(RESULT_OK, resultIntent);
         finish();
-
-    }
-
-    private String getDateAsString(Date date){
-        String twitterFormat = "EEE MMM dd HH:mm:ss ZZZZZ yyyy";
-        SimpleDateFormat sf = new SimpleDateFormat(twitterFormat, Locale.ENGLISH);
-        sf.setLenient(true);
-        return  sf.format(date);
     }
 }
